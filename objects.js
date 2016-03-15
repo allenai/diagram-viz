575b2d68a84eb15893611061e45456bcd2a7e83e
Jayant Krishnamurthy <jayantk@allenai.org>
2016-03-15 13:24:18 -0700

    canvas rendering and object selection

---
+++ objects.js
@@ -0,0 +1,126 @@
+
+function Blob(id, score, topleft, bottomright) {
+    this.id = id;
+    this.score = score;
+    this.topleft = topleft;
+    this.bottomright = bottomright;
+
+    this.type = "blob";
+    this.selected = false;
+
+    this.draw = function (c) {
+	c.beginPath();
+	c.font = "12px Arial";
+	c.strokeStyle = "rgba(0, 0, 0, 255)";
+	c.fillStyle = "rgba(0, 0, 0, 255)";
+	c.lineWidth = "1";
+
+	c.rect(topleft[1] * IMG_SCALE, topleft[0]  * IMG_SCALE,
+	       (bottomright[1] - topleft[1]) * IMG_SCALE,
+	       (bottomright[0] - topleft[0]) * IMG_SCALE);
+	c.fillText(this.id, (topleft[1] * IMG_SCALE) + 2, (topleft[0] * IMG_SCALE) + 12);
+	c.stroke();
+    };
+}
+
+function Text(id, score, value, topleft, bottomright) {
+    this.id = id;
+    this.score = score;
+    this.value = value;
+    this.topleft = topleft;
+    this.bottomright = bottomright;
+
+    this.type = "text";
+    this.selected = false;
+
+    this.draw = function (c) {
+	c.beginPath();
+	c.font = "12px Arial";
+	c.strokeStyle = "rgba(0, 0, 0, 255)";
+	c.fillStyle = "rgba(0, 0, 0, 255)";
+	c.lineWidth = "1";
+
+	c.rect(topleft[1] * IMG_SCALE, topleft[0]  * IMG_SCALE,
+	       (bottomright[1] - topleft[1]) * IMG_SCALE,
+	       (bottomright[0] - topleft[0]) * IMG_SCALE);
+	c.fillText(this.id, (topleft[1] * IMG_SCALE) + 2, (topleft[0] * IMG_SCALE) + 12);
+	c.stroke();
+    };
+}
+
+function Arrow(id, score, detection_yx) {
+    this.id = id;
+    this.score = score;
+    this.detection_yx = detection_yx;
+
+    this.type = "arrow";
+    this.selected = false;
+
+    this.draw = function (context) {
+	if (this.selected == true) {
+	    context.font = "16px Arial";
+	    context.fillStyle = "blue";
+	    context.strokeStyle = "blue";
+	    context.lineWidth = "3";
+	} else {
+	    context.font = "12px Arial";
+	    context.fillStyle = "red";
+	    context.strokeStyle = "red";
+	    context.lineWidth = "1";
+	}
+
+	var detection = this.detection_yx;
+	if (detection.length > 0) {
+	    context.fillText(this.id, detection[0][1] * IMG_SCALE, detection[0][0] * IMG_SCALE);
+
+	    context.beginPath();
+	    context.moveTo(detection[0][1] * IMG_SCALE, detection[0][0] * IMG_SCALE);
+	    for (var i = 0; i < detection.length; i++) {
+		yx = detection[i];
+		context.lineTo(yx[1] * IMG_SCALE, yx[0] * IMG_SCALE);
+		context.moveTo(yx[1] * IMG_SCALE, yx[0] * IMG_SCALE);
+	    }
+	    context.stroke();
+	}
+    };
+}
+
+function ArrowHead(id, score, topleft, bottomright, angle) {
+    this.id = id;
+    this.score = score;
+    this.topleft = topleft;
+    this.bottomright = bottomright;
+    this.angle = angle;
+
+    this.type = "arrowHead";
+    this.selected = false;
+
+    this.draw = function (c) {
+	c.font = "12px Arial";
+	c.strokeStyle = "green";
+	c.fillStyle = "green";
+	
+	var centerx = (this.topleft[1] + this.bottomright[1]) / 2;
+	var centery = (this.topleft[0] + this.bottomright[0]) / 2;
+
+	// var centerx = ah["yxCentroid"][1];
+	// var centery = ah["yxCentroid"][0];
+	
+	c.fillText(this.id, centerx * IMG_SCALE + 4, centery * IMG_SCALE - 4);
+
+	// Draw the direction of the arrow:
+	var angle = 2 * Math.PI * this.angle / 360;
+	var arrowlength = 10;
+	var radius = 3;
+	var xcomp = Math.cos(angle) * arrowlength;
+	var ycomp = Math.sin(angle) * arrowlength;
+
+	c.beginPath();
+	c.moveTo(centerx * IMG_SCALE, centery * IMG_SCALE);
+	c.arc(centerx * IMG_SCALE, centery * IMG_SCALE, radius, 0, 2 * Math.PI);
+	c.fill();
+	c.moveTo(centerx * IMG_SCALE, centery * IMG_SCALE);
+	c.lineTo( (centerx + xcomp) * IMG_SCALE, (centery + ycomp) * IMG_SCALE);
+	c.stroke();
+    };
+}
--- scripts.js
+++ scripts.js
@@ -21,65 +21,131 @@ var getFileObject = function(filePathOrUrl, cb) {
     });
 };
 
+function parseJson(j) {
+    constituents = theJson["constituents"];
+    drawnObjects = [];
+
+    var blobs = constituents["blobs"];
+    for (var blobId in blobs) {
+	if (blobs.hasOwnProperty(blobId)) {
+	    var blob = blobs[blobId];
+	    drawnObjects.push(new Blob(blobId, blob["score"], blob["bbox"]["topleftyx"],
+				       blob["bbox"]["bottomrightyx"]));
+	}
+    }
+
+    var texts = constituents["text"];
+    for (var textId in texts) {
+	if (texts.hasOwnProperty(textId)) {
+	    var text = texts[textId];
+	    drawnObjects.push(new Text(textId, text["score"], text["value"],
+				       text["bbox"]["topleftyx"], text["bbox"]["bottomrightyx"]));
+	}
+    }
+
+    var arrows = constituents["arrows"];
+    for (var arrowId in arrows) {
+	if (arrows.hasOwnProperty(arrowId)) {
+	    var arrow = arrows[arrowId];
+	    drawnObjects.push(new Arrow(arrowId, arrow["score"], arrow["detection"]));
+	}
+    }
+
+    var arrowheads = constituents["arrowHeads"];
+    for (var id in arrowheads) {
+	if (arrowheads.hasOwnProperty(id)) {
+	    var ah = arrowheads[id];
+	    drawnObjects.push(new ArrowHead(id, ah["score"], ah["bbox"]["topleftyx"],
+					    ah["bbox"]["bottomrightyx"], ah["angle"]));
+	}
+    }
+}
+
 // Renders the content of the loaded json file.
 function displayContents(contents) {
     var j = JSON.parse(contents);
     theJson = j;
+    parseJson(theJson);
 
+    init();
     redraw();
 
     var element = document.getElementById('loading-success');
     element.innerHTML = "success";
 }
 
-function redraw() {
-    // Clear the canvas.
+function init() {
     var elt = document.getElementById('arrow-canvas');
     elt.width = imgWidth * IMG_SCALE;
     elt.height = imgHeight * IMG_SCALE;
+
+    elt.onmousedown = canvasMouseDown;
+
+    var hidden = document.getElementById('hidden-canvas');
+    hidden.width = imgWidth * IMG_SCALE;
+    hidden.height = imgHeight * IMG_SCALE;
+}
+
+function redraw() {
+    // Clear the canvas.
+    var elt = document.getElementById('arrow-canvas');
     var c = elt.getContext("2d");
     c.clearRect(0, 0, elt.width, elt.height);
 
-    // Draw elements
-    constituents = theJson["constituents"];
-    blobs = constituents["blobs"];
-
-    var overlayHolder = document.getElementById('overlay-holder');
-    overlayHolder.innerHTML = "";
-
     var blobThreshold = document.getElementById('blob-slider').value / 100;
-    document.getElementById('blob-score-threshold').innerHTML = blobThreshold;
-    overlayHolder.innerHTML += drawBboxes(constituents["blobs"], blobThreshold, "blob");
+    var arrowHeadThreshold = document.getElementById('arrowhead-slider').value / 100;
+    var arrowThreshold = document.getElementById('arrow-slider').value / 100;
+    var textThreshold = document.getElementById('text-slider').value / 100;
+
+    config['thresholds'] = new Object();
+    config['thresholds']['blob'] = blobThreshold;
+    config['thresholds']['arrowHead'] = arrowHeadThreshold;
+    config['thresholds']['arrow'] = arrowThreshold;
+    config['thresholds']['text'] = textThreshold;
+
+    // Draw elements
+    drawObjects(drawnObjects, config);
+
+    // Show object counts
+    var constituents = theJson["constituents"];
+
+    var blobs = constituents["blobs"];
     document.getElementById('num-blobs').innerHTML = count(blobs, -1);
     document.getElementById('num-displayed-blobs').innerHTML = count(blobs, blobThreshold);
 
     var arrowHeads = constituents["arrowHeads"];
-    var arrowHeadThreshold = document.getElementById('arrowhead-slider').value / 100;
-    document.getElementById('arrowhead-score-threshold').innerHTML = arrowHeadThreshold;
-    // overlayHolder.innerHTML += drawBboxes(constituents["arrowHeads"], arrowHeadThreshold, "arrowhead");
-    drawArrowHeads(arrowHeads, arrowHeadThreshold);
     document.getElementById('num-arrowheads').innerHTML = count(arrowHeads, -1);
     document.getElementById('num-displayed-arrowheads').innerHTML = count(arrowHeads, arrowHeadThreshold);
 
     var arrows = constituents["arrows"];
-    var arrowThreshold = document.getElementById('arrow-slider').value / 100;
-    document.getElementById('arrow-score-threshold').innerHTML = arrowThreshold;
-    drawArrows(arrows, arrowThreshold);
     document.getElementById('num-arrows').innerHTML = count(arrows, -1);
     document.getElementById('num-displayed-arrows').innerHTML = count(arrows, arrowThreshold);
-    
 
     var texts = constituents["text"];
-    var textThreshold = document.getElementById('text-slider').value / 100;
-    document.getElementById('text-score-threshold').innerHTML = textThreshold;
-    overlayHolder.innerHTML += drawBboxes(texts, textThreshold, "text");
     document.getElementById('num-texts').innerHTML = count(texts, -1);
     document.getElementById('num-displayed-texts').innerHTML = count(texts, textThreshold);
 
+    // Show interobject linkages if an arrow is selected
+    var iolElt = document.getElementById('links');
+    iolElt.innerHTML = "";
+    if (selectedObject >= 0 && drawnObjects[selectedObject].type == "arrow") {
+	var arrowId = drawnObjects[selectedObject].id;
+	var iols = theJson["relationships"]["interObjectLinkage"];
 
-    // overlayHolder.innerHTML += drawBboxes(constituents["arrowHeads"], 0);
-    // overlayHolder.innerHTML += drawBboxes(constituents["arrows"]);
-    // overlayHolder.innerHTML += drawBboxes(constituents["text"], 0);
+	for (var id in iols) {
+	    if (iols.hasOwnProperty(id)) {
+		var iol = iols[id];
+		var parts = id.split("+");
+		if (parts[1] == arrowId && iol.hasDirectionality == true) {
+		    var src = iol["origin"];
+		    var dst = iol["destination"];
+		    var score = iol["score"];
+		    
+		    iolElt.innerHTML += "<p>" + score + " " + src + " " + arrowId + " " + dst + "</p>";
+		}
+	    }
+	}
+    }
 }
 
 function count(dict, scoreThreshold) {
@@ -94,116 +160,95 @@ function count(dict, scoreThreshold) {
     return count;
 }
 
-function drawBboxes(blobs, scoreThreshold, classId) {
-    html = "";
-    for (var blobId in blobs) {
-	if (blobs.hasOwnProperty(blobId)) {
-	    var blob = blobs[blobId];
-	    if (blob["score"] > scoreThreshold) {
-		html += getOverlayHtml(blob, blobId, classId);
-	    }
-	}
-    }
-    return html;
-}
-
-
-function drawArrows(arrows, scoreThreshold) {
+function drawObjects(objects, config) {
     var elt = document.getElementById('arrow-canvas');
     var c = elt.getContext("2d");
-    c.font = "12px Arial";
-    c.fillStyle = "red";
-    for (var arrowId in arrows) {
-	if (arrows.hasOwnProperty(arrowId)) {
-	    var arrow = arrows[arrowId];
-	    if (arrow["score"] > scoreThreshold) {
-		var detection = arrow["detection"];
-		if (detection.length == 0) {
-		    continue;
-		}
-
-		c.fillText(arrowId, detection[0][1] * IMG_SCALE, detection[0][0] * IMG_SCALE);
-		
-		c.beginPath();
-		c.strokeStyle="red";
-		c.moveTo(detection[0][1] * IMG_SCALE, detection[0][0] * IMG_SCALE);
-		for (i = 0; i < detection.length; i++) {
-		    yx = detection[i];
-		    c.lineTo(yx[1] * IMG_SCALE, yx[0] * IMG_SCALE);
-		    c.moveTo(yx[1] * IMG_SCALE, yx[0] * IMG_SCALE);
-		}
-		c.stroke();
-	    }
+    for (var j = 0; j < objects.length; j++) {
+	var object = objects[j];
+	if (object.score > config['thresholds'][object.type]) {
+	    object.draw(c);
 	}
     }
 }
 
-function drawArrowHeads(arrowheads, scoreThreshold) {
-    var elt = document.getElementById('arrow-canvas');
-    var c = elt.getContext("2d");
-    c.font = "12px Arial";
-    c.fillStyle = "green";
-    for (var id in arrowheads) {
-	if (arrowheads.hasOwnProperty(id)) {
-	    var ah = arrowheads[id];
+function canvasMouseDown(e) {
+    mouseXy = getMouseXy(e);
+    x = mouseXy[0];
+    y = mouseXy[1];
 
-	    if (ah["score"] > scoreThreshold) {
-		var bbox = ah["bbox"];
-		var topleft = bbox["topleftyx"];
-		var bottomright = bbox["bottomrightyx"];
-
-		var centerx = (topleft[1] + bottomright[1]) / 2;
-		var centery = (topleft[0] + bottomright[0]) / 2;
-
-		// var centerx = ah["yxCentroid"][1];
-		// var centery = ah["yxCentroid"][0];
-		
-		c.fillText(id, centerx * IMG_SCALE + 4, centery * IMG_SCALE - 4);
-
-		// Draw the direction of the arrow:
-		var angle = 2 * Math.PI * ah["angle"] / 360;
-		var arrowlength = 10;
-		var radius = 3;
-		var xcomp = Math.cos(angle) * arrowlength;
-		var ycomp = Math.sin(angle) * arrowlength;
-
-		c.beginPath();
-		c.strokeStyle="green";
-		c.fillStyle="green";
-		c.moveTo(centerx * IMG_SCALE, centery * IMG_SCALE);
-		c.arc(centerx * IMG_SCALE, centery * IMG_SCALE, radius, 0, 2 * Math.PI);
-		c.fill();
-		c.moveTo(centerx * IMG_SCALE, centery * IMG_SCALE);
-		c.lineTo( (centerx + xcomp) * IMG_SCALE, (centery + ycomp) * IMG_SCALE);
-		c.stroke();
-	    }
+    var ind = selectObject(x, y, drawnObjects, config);
+    if (ind != selectedObject) {
+	if (ind >= 0) {
+	    drawnObjects[ind].selected = true;
 	}
+	if (selectedObject >= 0) {
+	    drawnObjects[selectedObject].selected = false;
+	}
+	selectedObject = ind;
+
+	redraw();
     }
-}
 
-function getOverlayHtml(blob, id, classId) {
-    var bbox = blob["bbox"];
-    var topleft = bbox["topleftyx"];
-    var bottomright = bbox["bottomrightyx"];
-
-    var top = topleft[0] * IMG_SCALE;
-    var left = topleft[1] * IMG_SCALE;
-    var height = (bottomright[0] - topleft[0]) * IMG_SCALE;
-    var width = (bottomright[1] - topleft[1]) * IMG_SCALE;
-
-    style = "position: absolute; left: " + left + "px; top: " + top + "px; "
-    style += "width: " + width + "px; height: " + height + "px;"
-
-    var value = "";
-    /*
-    if (blob.hasOwnProperty("value")) {
-	value = blob["value"];
-    }
-    */
     
-    return "<div class=\"" + classId + "\" style=\"" + style + "\">" + id + " " + value + "</div>";
+    /*
+    var elt = document.getElementById('arrow-canvas');
+    var c = elt.getContext("2d");
+
+    c.beginPath();
+    c.strokeStyle="blue";
+    c.fillStyle="blue";
+    c.moveTo(x, y);
+    c.arc(x, y, 2, 0, 2 * Math.PI);
+    c.fill();
+    c.stroke();
+    */
 }
 
+function getMouseXy(e) {
+    var element = document.getElementById('arrow-canvas');
+
+    // For some reason there's an extra two pixel gap.
+    var offsetX = 2;
+    var offsetY = 2;    
+    if (element.offsetParent) {
+	do {
+	    offsetX += element.offsetLeft;
+	    offsetY += element.offsetTop;
+	} while ((element = element.offsetParent));
+    }
+    
+    return [e.pageX - offsetX, e.pageY - offsetY];
+}
+
+function selectObject(x, y, objects, config) {
+    var elt = document.getElementById('hidden-canvas');
+    var c = elt.getContext("2d");
+    c.clearRect(0, 0, elt.width, elt.height);
+
+    var selection = -1;
+    for (var i = 0; i < objects.length; i++) {
+	var object = objects[i];
+
+	if (object.score > config['thresholds'][object.type]) {
+	    object.draw(c);
+	    var imageData = c.getImageData(x, y, 1, 1);
+
+	    if (imageData.data[3] > 0) {
+		selection = i;
+		break;
+	    }
+	}
+    }
+
+    var selectionElt = document.getElementById('selected-object');
+    if (selection >= 0) {
+	selectionElt.innerHTML = objects[selection].id;
+    } else {
+	selectionElt.innerHTML = "none";
+    }
+    
+    return selection;
+}
 
 // global parameters
 var IMG_SCALE=2;
@@ -241,6 +286,11 @@ img.src = imageFilename;
 
 // Variable for storing the result of loading the JSON
 var theJson = null;
+// Variable storing the objects drawn on the canvas
+var drawnObjects = null;
+var selectedObject = -1;
+// Drawing configuration
+var config = new Object();
 
 getFileObject(jsonFilename, function (fileObject) {
     var reader = new FileReader();
